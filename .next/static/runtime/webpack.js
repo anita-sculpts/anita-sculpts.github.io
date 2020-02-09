2c66776e5cd5e3bfd817bd35b217eab06ef24586
anita-sculpts <anita-sculpts@protonmail.com>
2020-02-09 18:08:27 -0500

    support wide photos

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bd38fe3b04e42d3a043b";
+/******/ 	var hotCurrentHash = "0fd53971073b14adb8c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
