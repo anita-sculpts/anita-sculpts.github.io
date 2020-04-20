79c11707ad03a34eb2d0f5102e728c1bf224c98a
anita-sculpts <anita-sculpts@protonmail.com>
2020-04-20 13:10:58 -0400

    Add thumbnails to JSON

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5dab9c6a17be47e19d6e";
+/******/ 	var hotCurrentHash = "1f0980c038d32a9cda7e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
